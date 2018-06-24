0a1fee5762d09344e105da1eb22f9f88d5c3e206
manojkmishra <manojmishra571980@gmail.com>
2018-06-25 08:51:23 +1000

    test and dev profile created and working in ormconfig and package.json

--- src/utils/createTypeormConn.ts
+++ src/utils/createTypeormConn.ts
@@ -1,9 +1,9 @@
 import { getConnectionOptions, createConnection } from "typeorm";
 
 export const createTypeormConn = async () => {
-    console.log('its inside the createtypeormconn' );
+  //   console.log('its inside the createtypeormconn' );
     console.log('process.env.NODE_ENV=',process.env.NODE_ENV );
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV); // pass the ENV from package.json
-  console.log('connectionOptions=',connectionOptions );
+ //  console.log('connectionOptions=',connectionOptions );
   return createConnection({ ...connectionOptions, name: "default" });
 };
