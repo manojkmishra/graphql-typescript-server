47ff3d1800b4cff67bf8d46e47463c821ec78cfb
manojkmishra <manojmishra571980@gmail.com>
2018-06-24 14:22:14 +1000

    add bcrypt and hash the password in /src/resolvers.ts

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,10 +1,18 @@
 import { ResolverMap } from "./types/graphql-utils";
+import * as bcrypt from "bcryptjs";
+import {User} from './entity/User'; 
 
-export const resolvers: ResolverMap = {
+export const resolvers: ResolverMap = 
+{
   Query: {
     hello: (_, { name }: GQL.IHelloOnQueryArguments) => `Bye ${name || "World"}`
   },
   Mutation: {
-    register: (_, { email, password }: GQL.IRegisterOnMutationArguments) => {  return email + password;  }
+    register: async (_, { email, password }: GQL.IRegisterOnMutationArguments) => 
+    { const hashedPassowrd = await bcrypt.hash(password, 10); //this is async---but we are waiting(ie wait till its done--then move ahead)
+      await User.create({email, password:hashedPassowrd }); //await added---so wait till its finished
+      return true;
+     // return email + password;  
+    }
   }
 };
