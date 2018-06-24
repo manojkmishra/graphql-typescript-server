b43f2547d219aacb708cf6df30fae1f270759b55
manojkmishra <manojmishra571980@gmail.com>
2018-06-24 17:28:10 +1000

    user getting inserted in db after yarn test

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,2 +1,13 @@
-function sum(a, b) {  return a + b;}
-test('adds 1 + 2 to equal 3', () => {  expect(sum(1, 2)).toBe(3);});
+// function sum(a, b) {  return a + b;}
+// test('adds 1 + 2 to equal 3', () => {  expect(sum(1, 2)).toBe(3);});
+// import {startServer} from "..";
+import { request } from "graphql-request";
+import { host } from "./constants";
+
+const email="aa@aa.com"; const password="aa"; 
+const mutation = `mutation {  register(email: "${email}", password: "${password}")  }`;
+test("Register user", async () =>
+{ // await startServer();
+  const response = await request(host, mutation);
+  expect(response).toEqual({ register: true });
+});
