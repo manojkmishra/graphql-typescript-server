7e3c3965d543b4bdb42c794aa6a93b0208939358
manojkmishra <manojmishra571980@gmail.com>
2018-06-27 19:42:28 +1000

    two tests pasing in login---email not found---email not confirmed

--- src/modules/login/login.test.ts
+++ src/modules/login/login.test.ts
@@ -1,16 +1,31 @@
 import { request } from "graphql-request";
-import { invalidLogin } from "./errorMessages";
+import { invalidLogin, confirmEmailError } from "./errorMessages";
+import { User } from "../../entity/User";
+import { createTypeormConn } from "../../utils/createTypeormConn";
 
 const email = "manojkm1980@gmail.com";
 const password = "jalksdf";
 
+const registerMutation = (e: string, p: string) => `mutation { register(email: "${e}", password: "${p}") {  path   message  }}`;
 const loginMutation = (e: string, p: string) => `mutation {  login(email: "${e}", password: "${p}") {    path    message  }}`;
+
+beforeAll(async () => {  await createTypeormConn();});
+
+const loginExpectError = async (e: string, p: string, errMsg: string) => 
+{  const response = await request(    process.env.TEST_HOST as string,    loginMutation(e, p)  );
+   expect(response).toEqual({ login: [ { path: "email", message: errMsg } ] });
+};
 
 describe("login", () => 
  {  test("email not found send back error", async () => 
-       {   const response = await request( process.env.TEST_HOST as string, loginMutation(email, password) );
-           expect(response).toEqual({  login:[{  message: invalidLogin, path: "email" }]  });
-       
-        });
+         {  await loginExpectError("bob@bob.com", "sdfsdfsdf", invalidLogin);  });
+    test("email not confirmed", async () => 
+         {  await request(   process.env.TEST_HOST as string,  registerMutation(email, password)  );
+            await loginExpectError(email, password, confirmEmailError);
+            await User.update({ email }, { confirmed: true });
+            await loginExpectError(email, "aslkdfjaksdljf", invalidLogin);
+            const response = await request( process.env.TEST_HOST as string, loginMutation(email, password) );
+            expect(response).toEqual({ login: null });
+         });
 
  });
