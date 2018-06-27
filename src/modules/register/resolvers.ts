b611c289117c9642263028955818bcb67febaeb2
manojkmishra <manojmishra571980@gmail.com>
2018-06-27 14:33:32 +1000

    databse droped , register resolver modified----before login moduel

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -6,9 +6,9 @@ import { User } from "../../entity/User";
 import * as yup from "yup";
 import { formatYupError } from "../../utils/formatYupError";
 import {  duplicateEmail,  emailNotLongEnough,  invalidEmail,  passwordNotLongEnough} from "./errorMessages";
-import { createConfirmEmailLink } from "../../utils/createConfirmEmailLink";
-import { sendEmail } from "../../utils/sendEmail";
-import { v4 } from "uuid";
+//import { createConfirmEmailLink } from "../../utils/createConfirmEmailLink";
+//import { sendEmail } from "../../utils/sendEmail";
+//import { v4 } from "uuid";
 
 const schema = yup.object().shape(
 {  email: yup.string().min(3, emailNotLongEnough).max(255).email(invalidEmail),
@@ -19,7 +19,7 @@ export const resolvers: ResolverMap =
 {
   Query:{bye: ()=>"bye"},
   Mutation: 
-  { register: async (_, args: GQL.IRegisterOnMutationArguments,{redis, url}) => 
+  { register: async (_, args: GQL.IRegisterOnMutationArguments) => 
     { try {   await schema.validate(args, { abortEarly: false });    } 
       catch (err) {  return formatYupError(err);
                    }
@@ -32,10 +32,10 @@ export const resolvers: ResolverMap =
       }
              
       const hashedPassowrd = await bcrypt.hash(password, 10); // this is async---but we are waiting(ie wait till its done--then move ahead)
-      const user= User.create({ id: v4(),email, password:hashedPassowrd }); // await added---so wait till its finished
+      const user= User.create({ email, password:hashedPassowrd }); // await added---so wait till its finished
       await user.save(); 
-      if (process.env.NODE_ENV !== "test") { await sendEmail(  email, await createConfirmEmailLink(url, user.id, redis)  ); }
-      await createConfirmEmailLink(url, user.id, redis);
+     // if (process.env.NODE_ENV !== "test") { await sendEmail(  email, await createConfirmEmailLink(url, user.id, redis)  ); }
+     // await createConfirmEmailLink(url, user.id, redis);
      // console.log('register-resolver-LInk=',link);
       return null; // changed to this from true in error handling
     }
--- src/testSetup/setup.ts
+++ src/testSetup/setup.ts
@@ -1,15 +1,15 @@
 import { startServer } from "../startServer";
+import { AddressInfo } from "net";
 
 
 export const setup = async () => {
    // let getHost1 = () => "";
+  // let getHost1 = () => "";
   const app = await startServer();
   // console.log('app====',app);
-  // const { port } = app.address();
-   const port = app.address();
+   const { port } = app.address() as AddressInfo;
+  // const port = app.address();
    console.log('port====',port);
- // process.env.TEST_HOST = `http://127.0.0.1:${port}`;
-  process.env.TEST_HOST = `http://127.0.0.1:4000`;
- 
- 
+  process.env.TEST_HOST = `http://127.0.0.1:${port}`;
+ // process.env.TEST_HOST = `http://127.0.0.1:4000`;
 };
