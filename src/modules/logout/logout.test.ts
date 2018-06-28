c8ea91c48e8da50507ec20e9539630cd1aae095d
manojkmishra <manojmishra571980@gmail.com>
2018-06-28 18:36:20 +1000

    multiple session logout test created

--- src/modules/logout/logout.test.ts
+++ src/modules/logout/logout.test.ts
@@ -16,22 +16,21 @@ beforeAll(async () =>
 });
 
 afterAll(async () => { conn.close();});
-// const loginMutation = (e: string, p: string) => `mutation {  login(email: "${e}", password: "${p}") {    path    message  }}`;
-// const meQuery = `{  me {    id    email  }}`;
-// const logoutMutation = `mutation {  logout}`;
 
 describe("logout", () => 
-{  test("test logging out a user", async () => 
+{ test("multiple sessions logout", async () => 
+     { const sess1 = new TestClient(process.env.TEST_HOST as string); // computer 1
+       const sess2 = new TestClient(process.env.TEST_HOST as string);     // computer 2
+       await sess1.login(email, password);   await sess2.login(email, password);
+       expect(await sess1.me()).toEqual(await sess2.me());
+       await sess1.logout();
+      expect(await sess1.me()).toEqual(await sess2.me());
+     });
+    test("test single session logout", async () => 
      {  const client = new TestClient(process.env.TEST_HOST as string);
         await client.login(email, password);
         const response = await client.me();
-       // await axios.post(  process.env.TEST_HOST as string,  {   query: loginMutation(email, password)  },  {  withCredentials: true  }  );
-       // const response = await axios.post(  process.env.TEST_HOST as string,  {  query: meQuery  },  {   withCredentials: true  }  );
-       // expect(response.data.data).toEqual({  me: { id: userId,  email   }  });
        expect(response.data).toEqual({  me: { id: userId,  email   }  });
-       // await axios.post( process.env.TEST_HOST as string, {  query: logoutMutation  },  {  withCredentials: true  }  );
-       // const response2 = await axios.post( process.env.TEST_HOST as string,  {  query: meQuery  },  { withCredentials: true  } );
-       //  expect(response2.data.data.me).toBeNull();
        await client.logout();
        const response2 = await client.me();
        expect(response2.data.me).toBeNull();
