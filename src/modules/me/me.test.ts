9988d5f1f0d1a906cf6b1a269b8656a0190bc440
manojkmishra <manojmishra571980@gmail.com>
2018-06-28 18:16:47 +1000

    me and register tests modified -replace axios with request-promise

--- src/modules/me/me.test.ts
+++ src/modules/me/me.test.ts
@@ -1,7 +1,8 @@
-import axios from "axios";
+// import axios from "axios";
 import { createTypeormConn } from "../../utils/createTypeormConn";
 import { User } from "../../entity/User";
 import { Connection } from "typeorm";
+import { TestClient } from "../../utils/TestClient";
 
 let userId: string;
 let conn: Connection;
@@ -16,24 +17,29 @@ beforeAll(async () =>
 
 afterAll(async () => { conn.close();});
 
-const loginMutation = (e: string, p: string) => `mutation {  login(email: "${e}", password: "${p}") {    path    message  }}`;
-
-const meQuery = `{  me {    id    email  }}`;
+//  loginMutation = (e: string, p: string) => `mutation {  login(email: "${e}", password: "${p}") {    path    message  }}`;
+// const meQuery = `{  me {    id    email  }}`;
 
 describe("me", () => 
 { // test("can't get user if not logged in", async () => {  // later  });
     test("return null if no cookie", async () => 
-    {   const response = await axios.post(process.env.TEST_HOST as string, { query: meQuery });
-        console.log('me.test1-response.data.data.me',response.data.data.me)
-        expect(response.data.data.me).toBeNull();
+    {  // const response = await axios.post(process.env.TEST_HOST as string, { query: meQuery });
+       // console.log('me.test1-response.data.data.me',response.data.data.me)
+       // expect(response.data.data.me).toBeNull();
+        const client = new TestClient(process.env.TEST_HOST as string);
+        const response = await client.me();
+        expect(response.data.me).toBeNull();
     });
   test("get current user", async () => 
-  {   await axios.post(  process.env.TEST_HOST as string,   {  query: loginMutation(email, password)   },    
-                                                            {  withCredentials: true   }
-                      );
-    const response = await axios.post(  process.env.TEST_HOST as string,  {  query: meQuery  },  { withCredentials: true  } );
-   
-    console.log('me.test2-response.data.data',response.data.data);
-    expect(response.data.data).toEqual({  me: { id: userId, email  } });
+  {  // await axios.post(  process.env.TEST_HOST as string,   {  query: loginMutation(email, password)   },    
+     //                                                       {  withCredentials: true   }
+      //                );
+   // const response = await axios.post(  process.env.TEST_HOST as string,  {  query: meQuery  },  { withCredentials: true  } );
+    const client = new TestClient(process.env.TEST_HOST as string);
+    await client.login(email, password);
+    const response = await client.me();
+    console.log('me.test2-response.data.data',response.data);
+    expect(response.data).toEqual({  me: { id: userId, email  } });
+   // expect(response.data.data).toEqual({  me: { id: userId, email  } });
   });
 });
--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -1,64 +1,60 @@
-import { request } from "graphql-request";
-
 import { User } from "../../entity/User";
 import { duplicateEmail, emailNotLongEnough, invalidEmail, passwordNotLongEnough} from "./errorMessages";
 import { createTypeormConn } from "../../utils/createTypeormConn";
 import { Connection } from "typeorm";
-/*
-let getHost = () => "";
-beforeAll(async () => 
-{ // await createTypeormConn(); 
-    const app=await startServer();
-    const { port } = app.address();
-    getHost = () => `http://127.0.0.1:${port}`;
-});
-*/
-const getHost=process.env.TEST_HOST as string;
-const email="bbbb@bb.com"; const password="bbb"; 
-const mutation = (e: string, p: string) => `
-    mutation { register(email: "${e}", password: "${p}") { path message} }`;
+import { TestClient } from "../../utils/TestClient";
+
+const email = "tom@bob.com";
+const password = "jalksdf";
 
 let conn: Connection;
-beforeAll(async () => {   conn = await createTypeormConn();    });
-afterAll(async () => {  conn.close();   });
-    
+beforeAll(async () => {  conn = await createTypeormConn();});
+afterAll(async () => {  conn.close();});
+
 describe("Register user", async () => 
-{ it("check for duplicate emails", async () => // it or test----it can be any of the two
-  { // --------------------error handling-----------------------------------
-    const response = await request(getHost, mutation(email, password));
-    expect(response).toEqual({ register: null });
-    const users = await User.find({ where: { email } });
-    expect(users).toHaveLength(1); // it should be unique user
-    const user = users[0]; // take first entry of the array
-    expect(user.email).toEqual(email);
-    expect(user.password).not.toEqual(password); // password should be hashed so it will not be equal to user entered value
-    // ---------------in case error-------duplicate email---------------
-    const response2: any = await request(getHost, mutation(email, password));
-    expect(response2.register).toHaveLength(1); // in case error---register will return an array--
-    expect(response2.register[0]).toEqual({ path: "email", message: duplicateEmail}); // first element of array will be email, second will be error
+{  it("check for duplicate emails", async () => 
+  {   const client = new TestClient(process.env.TEST_HOST as string);
+      // make sure we can register a user
+      const response = await client.register(email, password);
+      expect(response.data).toEqual({ register: null });
+      const users = await User.find({ where: { email } });
+      expect(users).toHaveLength(1);
+      const user = users[0];
+      expect(user.email).toEqual(email);
+      expect(user.password).not.toEqual(password);
+
+      const response2 = await client.register(email, password);
+      expect(response2.data.register).toHaveLength(1);
+      expect(response2.data.register[0]).toEqual({  path: "email",  message: duplicateEmail  });
   });
-  // -----------------------------------error handling---catch bad email
-  it("check bad email", async () => // ----------------we can put test or it anything of the two
-  { const response3: any = await request(getHost, mutation("b", password));
-  expect(response3).toEqual(
-     { register: [ { path: "email", message: emailNotLongEnough },
-                   { path: "email", message: invalidEmail }
-                  ] 
-    });
+
+  it("check bad email", async () => 
+  { const client = new TestClient(process.env.TEST_HOST as string);
+    const response3 = await client.register("b", password);
+    expect(response3.data).toEqual(
+         {  register: [{ path: "email", message: emailNotLongEnough  },
+                       { path: "email", message: invalidEmail   }
+                      ]
+        });
   });
-  // -----------------------------------------catch bad password
+
   it("check bad password", async () => 
-  { const response4: any = await request(getHost, mutation(email, "ad"));
-  expect(response4).toEqual({ register: [{ path: "password", message: passwordNotLongEnough } ]});
+  { // catch bad password
+    const client = new TestClient(process.env.TEST_HOST as string);
+    const response4 = await client.register(email, "ad");
+    expect(response4.data).toEqual(
+      {  register: [ {  path: "password", message: passwordNotLongEnough  } ]
+      });
   });
-// -----------------------------------------catch bad password and bad email
+
   it("check bad password and bad email", async () => 
-  { const response5: any = await request(getHost, mutation("df", "ad"));
-    expect(response5).toEqual(
-    { register: [{ path: "email", message: emailNotLongEnough },
-                  { path: "email", message: invalidEmail },
-                  { path: "password", message: passwordNotLongEnough }
-                ]
-    });
+  { const client = new TestClient(process.env.TEST_HOST as string);
+    const response5 = await client.register("df", "ad");
+    expect(response5.data).toEqual(
+      { register: [{ path: "email", message: emailNotLongEnough },
+                   {path: "email", message: invalidEmail  },
+                   {path: "password", message: passwordNotLongEnough }
+                  ]
+      });
   });
 });
